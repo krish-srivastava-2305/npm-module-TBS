e9d287c9a101e4fc92d2dec4af10ae19b9a0acb5
Krish Srivastava <krish.2022ug3018@iiitranchi.ac.in>
2025-01-18 19:08:52 +0530

    'update'

--- build/middlewares/auth.middleware.js
+++ build/middlewares/auth.middleware.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.authUser = void 0;
 const not_authorized_error_1 = require("../errors/not-authorized-error");
 const authUser = (req, res, next) => {
+    console.log(req.currentUser);
     if (!req.currentUser) {
         throw new not_authorized_error_1.NotAuthorizedError();
     }
--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,6 +2,7 @@ import { NotAuthorizedError } from "../errors/not-authorized-error";
 import { Request, Response, NextFunction } from "express";
 
 export const authUser = (req:Request, res: Response, next:NextFunction) => {
+    console.log(req.currentUser);
     if(!req.currentUser) {
         throw new NotAuthorizedError();
     }
